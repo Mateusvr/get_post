1ede024fb26791483e56a8de3d8a9ee6a9e02ce4
Mateusvr <105557712+Mateusvr@users.noreply.github.com>
2023-04-10 19:40:37 -0300

    Add files via upload

--- async_await.js
+++ async_await.js
@@ -1,10 +1,14 @@
-// const getPosts = async () => {
-//     let response = await fetch('https://jsonplaceholder.typicode.com/users');
-//     let data = await response.json();
+const header = {
+    'Content-Type': 'application/json; charset=UTF-8'
+}
 
-//     console.log(data)
-// }
-// getPosts();
+const getPosts = async () => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users');
+    let data = await response.json();
+
+    console.log(data)
+}
+//getPosts();
 
 
 const newPost = async () => {
@@ -26,13 +30,39 @@ const newPost = async () => {
             userId: 5 
     }
 }
-        const header = {
-            'Content-Type': 'application/json; charset=UTF-8'
-        }
+  
 
     let response = await fetch(url, options, header)
     let data = await response.json()
     console.log(data)
 }
 
-newPost();
+//newPost();
+
+const updatePost = async (id) => {
+
+    let url = `https://jsonplaceholder.typicode.com/posts/${id}`
+    let options = {
+        method: 'PUT',
+        body: 'body'
+    }           
+
+    let response = await fetch(url, options, header);
+    let json = await response.json()
+    console.log(json);
+}
+
+updatePost(2, {
+    title: 'Teste update',
+    body: 'teste update body',
+    userId: 10
+})
+
+const deletePost = async (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+    method: 'DELETE'});
+    console.log('deletado!')
+    
+}
+
+deletePost(3)
